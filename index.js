f756a15ef92440c1644dc44f9e6f5346b4028835
zwduan <zwduan@relaper.com>
2023-06-06 17:11:39 +0800

    feat: 运动追踪器

--- index.js
+++ index.js
@@ -1,17 +1,99 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const bodyParser = require('body-parser')
 require('dotenv').config()
+
+const { User, Practice, Log } = require('./server.js')
+
+app.use(bodyParser.urlencoded({ extended: 'false' }))
+app.use(bodyParser.json())
 
 app.use(cors())
 app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
-});
+})
 
+const createAndSaveUser = require('./server').createAndSaveUser
+const findUser = require('./server').findUser
 
+app.post('/api/users', function (req, res) {
+  createAndSaveUser(req.body, function (err, data) {
+    res.json({
+      username: data.username,
+      _id: data._id,
+    })
+  })
+})
 
+app.get('/api/users', function (req, res) {
+  findUser(function (err, data) {
+    res.json(
+      data.map((item) => {
+        return { username: item.username, _id: item._id }
+      })
+    )
+  })
+})
 
+const findLogs = require('./server').findLogs
+// /api/users/:_id/logs?[from][&to][&limit]
+app.get('/api/users/:_id/logs', (req, res) => {
+  if (!req.params._id) return
+  findLogs(req.params._id, req.query, function (err, data) {
+    if (err) return
+
+    const logs = data
+      ? data.map((item) => {
+          return {
+            description: item.description,
+            duration: item.duration,
+            date: new Date(item.date).toDateString(),
+          }
+        })
+      : []
+    res.json({
+      username: data[0] ? data[0].username : '',
+      count: data.length,
+      log: logs,
+    })
+  })
+})
+
+/**
+ * {
+  "_id": "647ee12750c71a07e5509fbd",
+  "username": "1111",
+  "date": "Tue Jun 06 2023",
+  "duration": 232,
+  "description": "1123123213"
+}
+ */
+const findUserById = require('./server').findUserById
+const createAndSavePractice = require('./server').createAndSavePractice
+app.post('/api/users/:_id/exercises', (req, res) => {
+  findUserById(req.params._id, function (err, user) {
+    if (err) return
+    createAndSavePractice(
+      {
+        userid: user._id,
+        username: user.username,
+        description: req.body.description,
+        duration: req.body.duration,
+        date: req.body.date
+          ? new Date(req.body.date)
+          : new Date().toDateString(),
+      },
+      function (err, data) {
+        res.json({
+          _id: data.userid,
+          username: data.username,
+        })
+      }
+    )
+  })
+})
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
---
+++ server.js
@@ -0,0 +1,91 @@
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+mongoose.connect(process.env.MONGO_URI)
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+})
+const PracticeSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  userid: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  duration: Number,
+  date: Date,
+})
+// const LogSchema = new mongoose.Schema({
+//   username: {
+//     type: String,
+//     required: true,
+//   },
+//   count: Number,
+//   log: [
+//     {
+//       description: String,
+//       duration: Number,
+//       date: Date,
+//     },
+//   ],
+// })
+
+let User = mongoose.model('User', UserSchema)
+let Practice = mongoose.model('Practice', PracticeSchema)
+// let Log = mongoose.model('Log', LogSchema)
+
+const createAndSaveUser = (user, done) => {
+  var janeFonda = User.create(user, function (err, data) {
+    if (err) return console.error(err)
+    done(null, data)
+  })
+}
+
+const findUser = (done) => {
+  User.find({}, function (err, data) {
+    if (err) return console.error(err)
+    done(null, data)
+  })
+}
+
+const findUserById = (_id, done) => {
+  User.findById(_id, function (err, data) {
+    if (err) return console.error(err)
+    done(null, data)
+  })
+}
+
+const createAndSavePractice = (practice, done) => {
+  var janeFonda = Practice.create(practice, function (err, data) {
+    if (err) return console.error(err)
+    done(null, data)
+  })
+}
+
+const findLogs = (_id, { from, to, limit }, done) => {
+  Practice.find({
+    userid: _id,
+    date: { $gte: from || new Date('1900-01-01'), $lt: to || new Date() },
+  })
+    .limit(Number(limit) || 9999)
+    .exec(function (err, data) {
+      if (err) return console.error(err)
+      done(null, data)
+    })
+}
+
+exports.User = User
+exports.Practice = Practice
+// exports.Log = Log
+exports.createAndSaveUser = createAndSaveUser
+exports.findUser = findUser
+exports.findUserById = findUserById
+exports.createAndSavePractice = createAndSavePractice
+exports.findLogs = findLogs
